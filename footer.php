25442db1a178ecf0f22601d309312af41ab74628
vaibhavdevpro2000 <vaibhav.devpro@gmail.com>
2026-05-09 00:42:35 +0530

    first push

---
+++ footer.php
@@ -0,0 +1,234 @@
+
+        <!-- FOOTER
+        ================================================== -->
+        <footer class="bg-secondary pt-2-6 pt-sm-6 pt-md-8">
+            <div class="container">
+                <div
+                    class="row justify-content-center mb-2-6 mb-md-6 mb-lg-10 border-bottom border-color-light-white pb-2-6 pb-md-6">
+                    <div class="col-lg-8 col-xxl-7">
+                        <h2 class="display-3 font-weight-800 text-uppercase mb-0 text-white">
+                            Premium Digital Solutions for Your Growth
+                        </h2>
+                    </div>
+                </div>
+                <div class="row mb-2-6 mb-sm-6 mb-md-8 mt-n2-9">
+                    <div class="col-lg-4 col-xxl-5 mt-2-9">
+                        <a href="contact.html" class="circle-btn01 animation-up-down white mb-5">Start Growing</a>
+                        <div class="cd-headline clip font-weight-400 text-primary d-block">
+                            <span class="cd-words-wrapper">
+                                <link rel="stylesheet" href="">
+                                <b class="is-visible">Design • Develop • Grow</b>
+                                <b>Modern Website Solutions.</b>
+                                <b>Premium Brand Identity.</b>
+                                <b>Result-Driven Marketing.</b>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-xxl-7 mt-2-9">
+                        <div class="row mt-n1-9">
+                            <div class="col-md-4 mt-1-9">
+                                <h3
+                                    class="text-uppercase letter-spacing-2 font-weight-800 h6 text-primary mb-4 d-block">
+                                    Our Office
+                                </h3>
+                                <address class="display-29 text-white opacity7 mb-sm-1-9 mb-md-7">
+                                    Lucknow, India
+                                </address>
+                                <div class="email">
+                                    <a href="tel:8853693416"
+                                        class="display-27 font-weight-600 text-white text-primary-hover">
+                                        +91 88536 93416
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 mt-1-9">
+                                <div class="ps-md-6 ps-xxl-8">
+                                    <h3
+                                        class="text-uppercase letter-spacing-2 font-weight-800 h6 text-primary mb-4 d-block">
+                                        Quick Links
+                                    </h3>
+                                    <ul class="list-unstyled list-style04 mb-0">
+                                        <li><a href="about.html">Our Story</a></li>
+                                        <li><a href="portfolio.html">Case Studies</a></li>
+                                        <li><a href="services.html">Services</a></li>
+                                        <li><a href="pricing.html">Pricing</a></li>
+                                        <li><a href="contact.html">Get Started</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-4 mt-1-9">
+                                <h3
+                                    class="text-uppercase letter-spacing-2 font-weight-800 h6 text-primary mb-4 d-block">
+                                    Follow Us
+                                </h3>
+                                <ul class="list-unstyled mb-0 social-icon-style4">
+                                    <li><a href="https://www.instagram.com/pixellogic_ddg" target="_blank"><i
+                                                class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row border-top border-color-light-white py-1-6">
+                    <div class="col-md-6 mb-4 mb-md-0">
+                        <div class="text-center text-md-start">
+                            <p class="text-white mb-0">&copy; <span class="current-year">&nbsp;</span> PixelLogic. All
+                                rights reserved.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="text-center text-md-end">
+                            <p class="mb-0 text-white">Built for Performance & Growth.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+
+    <!-- BUY THEME
+    ================================================== -->
+    <div class="buy-theme alt-font d-none d-lg-inline-block">
+        <a href="contact.html">
+            <i class="fas fa-rocket"></i><span>Start Your Project</span>
+        </a>
+    </div>
+
+    <div class="wlt-overlay-bg"></div>
+
+    <div class="wlt-sidebar-main" id="wlt-sidebar-main">
+
+        <div class="popular-theme alt-font d-none d-lg-inline-block wl-btn wlt-btn">
+            <a href="#!">
+                <i class="fa-solid fa-layer-group"></i><span>Our Agency Solutions</span>
+            </a>
+        </div>
+
+        <div class="wlt-sidebar-inner scrollbar-macosx" id="wlt-sidebar-inner">
+            <div class="wlt-banner-section">
+                <div class="wlt-heading">
+                    <h3><span>Design Solutions</span></h3>
+                </div>
+                <!-- Premium Web Design Focus -->
+                <a href="web-design-development.html">
+                    <span class="wlt-banner">
+                        <span class="wlt-img-hover">
+                            <img src="img/preview/flyweb-web-design-agency-html-template.jpg" alt="PixelLogic - Premium Web Design">
+                        </span>
+                        <span class="wlt-banner-info">
+                            <span class="wlt-banner-name">
+                                <span class="wlt-proj-name">Modern Web Design</span>
+                            </span>
+                            <span class="wlt-proj-price">
+                                <bdi>Premium</bdi>
+                            </span>
+                        </span>
+                    </span>
+                </a>
+
+                <div class="wlt-heading">
+                    <h3><span>Growth & Marketing</span></h3>
+                </div>
+                <!-- E-commerce & Branding Focus -->
+                <a href="digital-marketing.html">
+                    <span class="wlt-banner">
+                        <span class="wlt-img-hover">
+                            <img src="img/preview/topweb-web-design-agency-html-template.jpg" alt="PixelLogic - Digital Marketing">
+                        </span>
+                        <span class="wlt-banner-info">
+                            <span class="wlt-banner-name">
+                                <span class="wlt-proj-name">Ads & Lead Gen</span>
+                            </span>
+                            <span class="wlt-proj-price">
+                                <bdi>Results-Driven</bdi>
+                            </span>
+                        </span>
+                    </span>
+                </a>
+                
+                <a href="branding-creative-design.html">
+                    <span class="wlt-banner">
+                        <span class="wlt-img-hover">
+                            <img src="img/preview/prolio-business-consulting-html-template.jpg" alt="PixelLogic - Creative Branding">
+                        </span>
+                        <span class="wlt-banner-info">
+                            <span class="wlt-banner-name">
+                                <span class="wlt-proj-name">Brand Identity</span>
+                            </span>
+                            <span class="wlt-proj-price">
+                                <bdi>Creative</bdi>
+                            </span>
+                        </span>
+                    </span>
+                </a>
+
+                <div class="wlt-heading">
+                    <h3><span>Software Excellence</span></h3>
+                </div>
+                <!-- Custom Software & CRM Focus -->
+                <a href="app-software-solutions.html">
+                    <span class="wlt-banner">
+                        <span class="wlt-img-hover">
+                            <img src="img/preview/solutic-it-solutions-services-html-template.jpg" alt="PixelLogic - Software Solutions">
+                        </span>
+                        <span class="wlt-banner-info">
+                            <span class="wlt-banner-name">
+                                <span class="wlt-proj-name">CRM & SaaS Dev</span>
+                            </span>
+                            <span class="wlt-proj-price">
+                                <bdi>Scalable</bdi>
+                            </span>
+                        </span>
+                    </span>
+                </a>
+            </div>
+        </div>
+
+        <div class="wlt-bottom-button">
+            <a class="wlt-link" href="mailto:info@pixellogic.in">For premium digital solutions, reach out at - info@pixellogic.in</a>
+        </div>
+
+    </div>
+
+    <!-- SCROLL TO TOP
+    ================================================== -->
+    <div class="scroll-top-percentage"><span id="scroll-value"></span></div>
+
+    <!-- all js include start -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- popper js -->
+    <script src="js/popper.min.js"></script>
+
+    <!-- bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- scroll -->
+    <script src="js/jquery.scrollbar.min.js"></script>
+
+    <!-- jquery -->
+    <script src="js/core.min.js"></script>
+
+    <!-- search -->
+    <script src="search/search.js"></script>
+
+    <!-- custom scripts -->
+    <script src="js/main.js"></script>
+
+    <!-- form plugins js -->
+    <script src="quform/js/plugins.js"></script>
+
+    <!-- form scripts js -->
+    <script src="quform/js/scripts.js"></script>
+
+    <!-- all js include end -->
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- metas -->
+    <meta charset="utf-8">
+    <meta name="author" content="Website Layout">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="IT Solutions & Services HTML Template">
+    <meta name="description" content="Codera - IT Solutions & Services HTML Template">
+
+    <!-- title  -->
+    <title>PixelLogic | Premium Website Design & Digital Marketing Agency</title>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="img/logos/favicon.png">
+    <link rel="apple-touch-icon" href="img/logos/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/logos/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/logos/apple-touch-icon-114x114.png">
+
+    <!-- plugins -->
+    <link rel="stylesheet" href="css/plugins.css">
+
+    <!-- quform css -->
+    <link rel="stylesheet" href="quform/css/base.css">
+
+    <!-- scroll -->
+    <link rel="stylesheet" href="css/scrollbar.css">
+
+    <!-- theme core css -->
+    <link href="css/styles.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- PAGE LOADING
+    ================================================== -->
+    <div id="preloader"></div>
+
+    <!-- MAIN WRAPPER
+    ================================================== -->
+    <div class="main-wrapper">
+
+        <!-- HEADER
+        ================================================== -->
+        <header class="header-style1 menu_area-light">
+
+            <div class="navbar-default border-bottom border-color-light-white">
+
+                <!-- start top search -->
+                <div class="top-search bg-primary">
+                    <div class="container-fluid px-lg-1-6 px-xl-2-5 px-xxl-2-9">
+                        <form class="search-form" action="search.html" method="GET" accept-charset="utf-8">
+                            <div class="input-group">
+                                <span class="input-group-addon cursor-pointer">
+                                    <button class="search-form_submit fas fa-search text-white" type="submit"></button>
+                                </span>
+                                <input type="text" class="search-form_input form-control" name="s" autocomplete="off"
+                                    placeholder="Type & hit enter...">
+                                <span class="input-group-addon close-search mt-1"><i class="fas fa-times"></i></span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- end top search -->
+
+                <div class="container-fluid px-lg-1-6 px-xl-2-5 px-xxl-2-9">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-lg-12">
+                            <div class="menu_area alt-font">
+                                <nav class="navbar navbar-expand-lg navbar-light p-0">
+                                    <div class="navbar-header navbar-header-custom">
+                                        <!-- start logo -->
+                                        <a href="index.html" class="navbar-brand logochange"><img id="logo"
+                                                src="img/logos/logo.png" alt="logo"></a>
+                                        <!-- end logo -->
+                                    </div>
+
+                                    <div class="navbar-toggler bg-primary"></div>
+
+                                    <!-- menu area -->
+                                    <ul class="navbar-nav ms-auto" id="nav" style="display: none;">
+                                        <li>
+                                            <a href="index.html">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="about.html">About</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Services</a>
+                                            <ul>
+                                                <li><a href="web-design-development.html">Website Design & Dev</a></li>
+                                                <li><a href="digital-marketing.html">Digital Marketing</a></li>
+                                                <li><a href="branding-creative.html">Branding & Creative</a></li>
+                                                <li><a href="app-software-solutions.html">App & Software Solutions</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">Solutions</a>
+                                            <ul>
+                                                <li><a href="ecommerce-websites.html">E-commerce Stores</a></li>
+                                                <li><a href="landing-pages.html">High-Converting Landing Pages</a></li>
+                                                <li><a href="crm-erp-solutions.html">Custom CRM / ERP</a></li>
+                                                <li><a href="business-automation.html">Business Automation</a></li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="#">Portfolio</a>
+                                            <ul>
+                                                <li><a href="portfolio-grid.html">Project Gallery</a></li>
+                                                <li><a href="case-studies.html">Case Studies</a></li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <a href="pricing.html">Pricing</a>
+                                        </li>
+                                        <li>
+                                            <a href="contact.html">Contact</a>
+                                        </li>
+                                    </ul>
+                                    <!-- end menu area -->
+
+                                    <!-- start attribute navigation -->
+                                    <div class="attr-nav align-items-xl-center ms-xl-auto main-font">
+                                        <ul>
+                                            <li class="search"><a href="#"><i class="fas fa-search"></i></a></li>
+                                            <li class="d-none d-xl-inline-block"><a href="contact.html"
+                                                    class="butn-style03 sm"><span class="text-white">Free
+                                                        Quote</span></a></li>
+                                        </ul>
+                                    </div>
+                                    <!-- end attribute navigation -->
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </header>
